8f9d4c222ee200273ff2675ea0bbb1941f040c7a
quinm0 <quin@moran1.com>
2024-06-27 07:44:55 -0400

    add a server

---
+++ apps/library/src/serverWithoutHandler.ts
@@ -0,0 +1,11 @@
+import { env } from './env';
+import { LIBRARY_QUEUE_NAME, type LibraryJobData, type LibraryJobResult } from 'types';
+import { JobRuntimeManager } from 'jobruntime';
+
+export const runtime = new JobRuntimeManager<LibraryJobData, LibraryJobResult>({
+  queueName: LIBRARY_QUEUE_NAME,
+  host: env.REDIS_HOST,
+  port: env.REDIS_PORT,
+  debug: true,
+  debugInterval: 3000
+});
