d8ea05f1ac18465993077c185454366cb94818a6
quinm0 <quin@moran1.com>
2024-06-27 11:40:00 -0400

    test show directories vs files

--- apps/test/src/index.ts
+++ apps/test/src/index.ts
@@ -1,7 +1,12 @@
 import { libraryJM } from './shared';
 
-await libraryJM.fireAndWaitForJobResult('indexLibrary', {
+const result = await libraryJM.fireAndWaitForJobResult('indexLibrary', {
   directoryPath: './',
 });
 
+// log how many are directories and how many are files
+const numDirectories = result.files.filter(file => file.isDirectory).length;
+const numFiles = result.files.filter(file => !file.isDirectory).length;
+console.log(`Number of directories: ${numDirectories}`);
+console.log(`Number of files: ${numFiles}`);
 process.exit(0);
